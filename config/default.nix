525a4652befc6b506c4add1f5e1ccb96533c319f
TakodaS <austen.bolitho@protonmail.com>
2024-07-09 11:01:02 +0100

    added remote-nvim
    Remote-nvim is not in nixpkgs, if I like this plugin I will add it

--- config/default.nix
+++ config/default.nix
@@ -78,6 +78,7 @@
     ./utils/persistence.nix
     ./utils/plenary.nix
     ./utils/project-nvim.nix
+    ./utils/remote-nvim.nix
     ./utils/sidebar.nix
     ./utils/tmux-navigator.nix
     ./utils/todo-comments.nix
