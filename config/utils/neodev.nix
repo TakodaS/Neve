4e8fa32d5727f4e9a3d412d6ef479e372048b21d
TakodaS <austen.bolitho@protonmail.com>
2024-07-10 06:54:22 +0100

    changing from neodev to lazydev

--- config/utils/neodev.nix
+++ config/utils/neodev.nix
@@ -1,11 +1,18 @@
 {pkgs, ...}: {
-  extraPlugins = with pkgs.vimPlugins; [
-    neodev-nvim
+  extraPlugins = with pkgs.vimUtils; [
+    (buildVimPlugin {
+      pname = "lazydev.nvim";
+      version = "2024-07-10";
+      src = pkgs.fetchFromGitHub {
+        owner = "folke";
+        repo = "lazydev.nvim";
+        rev = "cea5d0fb556cdc35122d9cae772e7e0ed65b4505";
+        sha256 = "sha256-7GYIYg6YNmk1psZ02UCtlDiTg4rY7Dr4fcaaZIvbTGM=";
+      };
+    })
   ];
-
   extraConfigLua = ''
-    require("neodev").setup({
-      library = { plugins = {"neotest"}, types = true},
+    require("lazydev").setup({
     })
   '';
 }
